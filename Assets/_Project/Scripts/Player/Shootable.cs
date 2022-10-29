df6283ec8d50e7bdd09b44069a55cbd613219d1c
Rakib Jahan <rakib@lightneer.com>
2022-10-30 00:14:16 +0600

    shootable survive time, enemy hitpoints update

--- Assets/_Project/Scripts/Player/Shootable.cs
+++ Assets/_Project/Scripts/Player/Shootable.cs
@@ -6,11 +6,13 @@ namespace RakibJahan
 {
     public class Shootable : MonoBehaviour
     {
+        [SerializeField] private float shootableSurviveTime = 3f;
         private float _damagePerHit = 10;
 
         public void Init(float damagePerHit)
         {
             _damagePerHit = damagePerHit;
+            Invoke(nameof(DestroyShootable), shootableSurviveTime);
         }
 
         private void OnCollisionEnter(Collision collision)
@@ -19,8 +21,13 @@ namespace RakibJahan
             {
                 var damageable = collision.transform.GetComponentInChildren<IDamageable>();
                 damageable.Damage(_damagePerHit);
-                Destroy(gameObject);
+                DestroyShootable();
             }
+        }
+
+        private void DestroyShootable()
+        {
+            Destroy(gameObject);
         }
     }
 }
